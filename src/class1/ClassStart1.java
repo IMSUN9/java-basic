54d9c606001a6fb5cc64ce20515a64e44a1824be
Ralo <168403383+IMSUN9@users.noreply.github.com>
2025-01-12 15:18:08 +0900

    Init

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,20 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args)
+    {
+
+        String student1 = "학생1";
+        int age = 15;
+        int result = 90;
+        System.out.println("이름: " + student1 + " 나이: " + age + " 성적: " + result);
+
+        String student2 = "학생2";
+        int age1 = 16;
+        int result1 = 80;
+        System.out.println("이름: " + student2 + " 나이: " + age1 + " 성적: " + result1);
+
+
+    }
+}
