e2306cd649c32ca67066812e0716c7a65163214f
IMSUNGU <168403383+IMSUN9@users.noreply.github.com>
2025-01-24 22:37:19 +0900

    2025.01.24

--- src/access/ex/MaxCounter.java
+++ src/access/ex/MaxCounter.java
@@ -5,8 +5,26 @@ public class MaxCounter
     private int count = 0;
     private int max;
 
-    public MaxCounter(int max)
+    public MaxCounter(int max) // 생성자
     {
         this.max = max;
+    }
+
+    public void increment()
+    {
+        // 검증 로직
+        if (count >= max)
+        {
+            System.out.println("최댓값을 초과할 수 없습니다.");
+            return;
+        }
+
+        // 실행 로직
+        count++;
+    }
+
+    public int getCount()
+    {
+        return count;
     }
 }
